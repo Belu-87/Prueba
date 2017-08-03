b5bcdbca4ce68c414d985d0b90ce0e733eb80d51
Belu-87 <mbblasquiz@gmail.com>
2017-08-03 19:47:16 -0300

    segundo commit imagenes y docs agregado

---
+++ backup.sql
@@ -0,0 +1,280 @@
+CREATE DATABASE  IF NOT EXISTS `dulzuras_artesanales` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `dulzuras_artesanales`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dulzuras_artesanales
+-- ------------------------------------------------------
+-- Server version	5.7.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `direccion`
+--
+
+DROP TABLE IF EXISTS `direccion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `direccion` (
+  `id` int(11) NOT NULL,
+  `calle` varchar(45) DEFAULT NULL,
+  `numero` int(11) DEFAULT NULL,
+  `piso` int(11) DEFAULT NULL,
+  `departamento` varchar(45) DEFAULT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  `idUsuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `direccion`
+--
+
+LOCK TABLES `direccion` WRITE;
+/*!40000 ALTER TABLE `direccion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `direccion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `envio`
+--
+
+DROP TABLE IF EXISTS `envio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `envio` (
+  `id` int(11) NOT NULL,
+  `detalle` varchar(85) DEFAULT NULL,
+  `idDireccion` int(11) DEFAULT NULL,
+  `importe` decimal(8,2) DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='				';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `envio`
+--
+
+LOCK TABLES `envio` WRITE;
+/*!40000 ALTER TABLE `envio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `envio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `forma_de_pago`
+--
+
+DROP TABLE IF EXISTS `forma_de_pago`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `forma_de_pago` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `forma_de_pago`
+--
+
+LOCK TABLES `forma_de_pago` WRITE;
+/*!40000 ALTER TABLE `forma_de_pago` DISABLE KEYS */;
+/*!40000 ALTER TABLE `forma_de_pago` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedido`
+--
+
+DROP TABLE IF EXISTS `pedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pedido` (
+  `id` int(11) NOT NULL,
+  `numero` int(11) DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `idUsuario` int(11) DEFAULT NULL,
+  `idDireccion` int(11) DEFAULT NULL,
+  `total` decimal(8,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedido`
+--
+
+LOCK TABLES `pedido` WRITE;
+/*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedido_detalle`
+--
+
+DROP TABLE IF EXISTS `pedido_detalle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pedido_detalle` (
+  `id` int(11) NOT NULL,
+  `idPedido` int(11) DEFAULT NULL,
+  `idProductoPedido` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `precioUnitario` decimal(8,2) DEFAULT NULL,
+  `precioTotal` decimal(8,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedido_detalle`
+--
+
+LOCK TABLES `pedido_detalle` WRITE;
+/*!40000 ALTER TABLE `pedido_detalle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido_detalle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedido_producto_detalle`
+--
+
+DROP TABLE IF EXISTS `pedido_producto_detalle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pedido_producto_detalle` (
+  `id` int(11) NOT NULL,
+  `idPedidoProducto` int(11) DEFAULT NULL,
+  `idProductoDetalle` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedido_producto_detalle`
+--
+
+LOCK TABLES `pedido_producto_detalle` WRITE;
+/*!40000 ALTER TABLE `pedido_producto_detalle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido_producto_detalle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `producto`
+--
+
+DROP TABLE IF EXISTS `producto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `producto` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(85) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='															';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `producto`
+--
+
+LOCK TABLES `producto` WRITE;
+/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `producto_detalle`
+--
+
+DROP TABLE IF EXISTS `producto_detalle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `producto_detalle` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(85) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='		';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `producto_detalle`
+--
+
+LOCK TABLES `producto_detalle` WRITE;
+/*!40000 ALTER TABLE `producto_detalle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `producto_detalle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `producto_pedido`
+--
+
+DROP TABLE IF EXISTS `producto_pedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `producto_pedido` (
+  `id` int(11) NOT NULL,
+  `idProducto` int(11) DEFAULT NULL,
+  `idPedidoProductoDetalle` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `producto_pedido`
+--
+
+LOCK TABLES `producto_pedido` WRITE;
+/*!40000 ALTER TABLE `producto_pedido` DISABLE KEYS */;
+/*!40000 ALTER TABLE `producto_pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `alias` varchar(45) DEFAULT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `facebook` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-08-02 23:14:02
